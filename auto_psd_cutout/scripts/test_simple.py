537d02cfb489acb9f225be954a31c03cc01d320b
p1352966772-dot <p1352966772-dot@users.noreply.github.com>
2026-06-26 09:56:18 +0800

    chore: 清理废弃/调试/临时文件

--- auto_psd_cutout/scripts/test_simple.py
+++
@@ -1,7 +0,0 @@
-import sys, os
-sys.stdout.reconfigure(encoding="utf-8")
-src = r"C:\Users\Administrator.SK-20241009ZRBE\Desktop\KoutuTools\auto_psd_cutout\src"
-path = os.path.join(src, "rembg_utils.py")
-with open(path, "w", encoding="utf-8") as f:
-    f.write("x = 1\n")
-print("done")
